7f24da7f224f96265a92bd5c817c4e9193d979e6
ethan <ethanlijiahao@didiglobal.com>
2025-05-16 16:52:33 +0800

    feat: produce

--- producer/src/app.module.ts
+++ producer/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RabbitMqModule } from './rabbit/rabbit.module';
 import { ScheduleModule } from '@nestjs/schedule';
+import { RabbitNewModule } from './rabbit-new/rabbit-new.module';
 
 @Module({
-  imports: [RabbitMqModule, ScheduleModule.forRoot()],
+  imports: [RabbitMqModule, ScheduleModule.forRoot(), RabbitNewModule],
   controllers: [AppController],
   providers: [AppService],
 })
